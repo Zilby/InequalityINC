4fd7c5691bbe71b823e936bb686bb687da594adb
Zilby <azilby@gmail.com>
2018-02-15 17:55:31 -0500

    comments

--- Assets/Scripts/Utility/SpriteOrderer.cs
+++ Assets/Scripts/Utility/SpriteOrderer.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Sets the sprite render order so that sprites that are
+/// higher up are layered below sprites that are below. 
+/// </summary>
 public class SpriteOrderer : MonoBehaviour {
 
+	/// <summary>
+	/// The sprite renderer for this gameobject
+	/// </summary>
 	private SpriteRenderer s;
 
 	void Awake() {
