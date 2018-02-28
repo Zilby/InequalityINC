b9770208493da29b3426de078dbeda12fc935f99
SteveKrueger52 <stevekrueger52@gmail.com>
2018-02-28 13:21:59 -0500

    Added main menu, including music. Logging functionality is not currently present but the rest is.

---
+++ Assets/Scripts/PersistentGameData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentGameData : MonoBehaviour {
+    private static PersistentGameData instance;
+    public Dictionary<string, string> data = new Dictionary<string, string> ();
+
+    // Static singleton property
+    public static PersistentGameData Instance
+    {
+        // Here we use the ?? operator, to return 'instance' if 'instance' does not equal null
+        // otherwise we assign instance to a new component and return that
+        get { return instance ?? (instance = new GameObject ("[GameData]").AddComponent<PersistentGameData> ()); }
+    }
+
+    public void Awake() {
+        // Set default parameters for all required fields here
+    }
+}
---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+    // Values to track in PersistentGameData for scene transition
+    private bool logActive = false;
+    private string logOutput = "log.txt";
+    private string characterName = "Alex"; // Default name for the player.
+    private Text nameField;
+    private Text placeholder;
+
+
+    private GameObject controls;
+
+    // Initialization boilerplate stuff
+	void Awake () {
+        nameField = GameObject.Find("NameText").GetComponent<Text>();
+        placeholder = nameField.transform.parent.Find ("Placeholder").GetComponent<Text> ();
+        placeholder.text = characterName;
+
+        controls = GameObject.Find("Controls");
+        controls.SetActive (false);
+	}
+
+    // Called when the player name is updated from the default.
+    public void NameUpdated(string newName) {
+        if (newName == "")
+            characterName = placeholder.text;
+        else
+            characterName = newName;
+    }
+
+    // Called when the 'Play' button is pressed
+    public void ChangeScene() {
+        PersistentGameData.Instance.data.Add ("playerName", characterName);
+        PersistentGameData.Instance.data.Add ("logActive", logActive.ToString());
+        PersistentGameData.Instance.data.Add ("logOutput", logOutput);
+        Debug.Log ("Scene 'changed'"); //TODO
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1); // Loads main game scene
+    }
+
+    // Called when the 'Controls' button is pressed
+    public void ViewControls() {
+        Debug.Log ("view controls");
+        controls.SetActive (true);
+    }
+}
