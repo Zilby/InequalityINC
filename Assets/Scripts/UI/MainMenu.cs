10130b82117a01c153c3b7fb71f4a1960c48c3e7
Zilby <azilby@gmail.com>
2018-03-02 12:47:45 -0500

    refactored some of the menu code

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,61 +2,95 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
-public class MainMenu : MonoBehaviour {
-    private bool logActive = false; // Fields for the output log file.
-    private string logOutput; 
-    private Text logPlaceholder;
+/// <summary>
+/// Manages the main menu class. 
+/// </summary>
+public class MainMenu : MonoBehaviour
+{
+	public TMP_InputField nameField;
+	public TextMeshProUGUI namePlaceholder;
 
-    private string characterName; // Fields for the player name.
-    private Text namePlaceholder;
+	public Button playButton;
+	public Button controlsButton;
+	public Button controlsScreenButton;
 
-    private GameObject controls; // Controls Page gameObject.
+	/// <summary>
+	/// The controls page. 
+	/// </summary>
+	public FadeableUI controls;
 
-    // Initialization boilerplate stuff
-	void Awake () {
-        // Set default player name in text field
-        Text nameField = GameObject.Find("NameText").GetComponent<Text>();
-        namePlaceholder = nameField.transform.parent.Find ("Placeholder").GetComponent<Text> ();
-        namePlaceholder.text = Stats.PlayerName;
-        characterName = Stats.PlayerName;
+	private bool logActive = false; // Fields for the output log file.
+	private string logOutput;
+	private Text logPlaceholder;
 
-        // Catalog and hide the Controls page
-        controls = GameObject.Find("Controls");
-        controls.SetActive (false);
+	private string characterName; // Fields for the player name.
 
-        // Set default filepath for log file
-        Text logField = GameObject.Find("LogText").GetComponent<Text>();
-        logPlaceholder = logField.transform.parent.Find ("Placeholder").GetComponent<Text> ();
-        logPlaceholder.text = Stats.LogFile;
+	// Initialization boilerplate stuff
+	void Awake()
+	{
+		nameField.onEndEdit.AddListener((string arg) => NameUpdated(arg));
+		namePlaceholder.text = Stats.PlayerName;
 
-        // Set Logging checkmark to false
-        GameObject logToggle = GameObject.Find("OutputField");
-        logToggle.SetActive (false);
-        GameObject.Find ("LoggingToggle").GetComponent<Toggle> ().isOn = false;
+		playButton.onClick.AddListener(ChangeScene);
+		controlsButton.onClick.AddListener(ToggleControls);
+		controlsScreenButton.onClick.AddListener(ToggleControls);
+
+		characterName = Stats.PlayerName;
+
+		// Catalog and hide the Controls page
+		controls.Hide();
+
+		// Set default filepath for log file
+		Text logField = GameObject.Find("LogText").GetComponent<Text>();
+		logPlaceholder = logField.transform.parent.Find("Placeholder").GetComponent<Text>();
+		logPlaceholder.text = Stats.LogFile;
+
+		// Set Logging checkmark to false
+		GameObject logToggle = GameObject.Find("OutputField");
+		logToggle.SetActive(false);
+		GameObject.Find("LoggingToggle").GetComponent<Toggle>().isOn = false;
 	}
 
-    // Called when the player name is updated from the default.
-    public void NameUpdated(string newName) {
-        if (newName == "")
-            characterName = namePlaceholder.text;
-        else
-            characterName = newName;
-    }
+	// Called when the player name is updated from the default.
+	public void NameUpdated(string newName)
+	{
+		if (newName == "")
+			characterName = namePlaceholder.text;
+		else
+			characterName = newName;
+	}
 
-    // Called when the 'Play' button is pressed
-    public void ChangeScene() {
-        Stats.PlayerName = characterName;
-        Stats.LogFile = logOutput;
-        Stats.LogActive = logActive;
 
-        Debug.Log ("Scene 'changed'"); //TODO
-        UnityEngine.SceneManagement.SceneManager.LoadScene(1); // Loads main game scene
-    }
+	/// <summary>
+	/// Changes the scene when the 'Play' button is pressed.
+	/// </summary>
+	public void ChangeScene()
+	{
+		Stats.PlayerName = characterName;
+		Stats.LogFile = logOutput;
+		Stats.LogActive = logActive;
 
-    // Called when the 'Controls' button is pressed
-    public void ViewControls() {
-        Debug.Log ("view controls");
-        controls.SetActive (true);
-    }
+		Debug.Log("Scene 'changed'"); //TODO
+		UnityEngine.SceneManagement.SceneManager.LoadScene(1); // Loads main game scene
+	}
+
+	/// <summary>
+	/// Fades in and out the controls when the 'Controls' button is pressed. 
+	/// </summary>
+	public void ToggleControls()
+	{
+		if (!controls.IsFading)
+		{
+			if (controls.IsVisible)
+			{
+				controls.SelfFadeOut(dur: 0.15f);
+			}
+			else
+			{
+				controls.SelfFadeIn(dur: 0.15f);
+			}
+		}
+	}
 }
