1aea3ea5c83feec281cf24eb2fa746f2f1289aac
Zilby <azilby@gmail.com>
2018-03-20 13:02:25 -0400

    Added basic node system

---
+++ Assets/Scripts/DialogueSystem/ConnectionPoint.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+public enum ConnectionPointType
+{
+	In,
+	Out
+}
+
+public class ConnectionPoint
+{
+	public Rect rect;
+
+	public ConnectionPointType type;
+
+	public DialogueNode node;
+
+	public GUIStyle style;
+
+	public Action<ConnectionPoint> OnClickConnectionPoint;
+
+	public ConnectionPoint(DialogueNode node, ConnectionPointType type, GUIStyle style, Action<ConnectionPoint> OnClickConnectionPoint)
+	{
+		this.node = node;
+		this.type = type;
+		this.style = style;
+		this.OnClickConnectionPoint = OnClickConnectionPoint;
+		rect = new Rect(0, 0, 20f, 10f);
+	}
+
+	public void Draw()
+	{
+		rect.x = node.rect.x + (node.rect.width * 0.5f) - rect.width * 0.5f;
+
+		switch (type)
+		{
+			case ConnectionPointType.In:
+				rect.y = node.rect.y + (node.rect.height * 0.5f) - rect.height * 0.5f - 22;
+				break;
+
+			case ConnectionPointType.Out:
+				rect.y = node.rect.y + (node.rect.height * 0.5f) - (rect.height * 0.5f) + 20;
+				break;
+		}
+
+		if (GUI.Button(rect, "", style))
+		{
+			if (OnClickConnectionPoint != null)
+			{
+				OnClickConnectionPoint(this);
+			}
+		}
+	}
+}
