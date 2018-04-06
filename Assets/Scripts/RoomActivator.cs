2f0d1dd4aee85de1e4f7ef3ac14333b5eb216dc5
Zilby <azilby@gmail.com>
2018-04-06 13:50:54 -0400

    Break room added

---
+++ Assets/Scripts/RoomActivator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomActivator : MonoBehaviour {
+
+	public float top;
+	public float bottom;
+
+	private static List<RoomActivator> rooms = new List<RoomActivator>();
+
+	private void Awake()
+	{
+		rooms.Add(this);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float y = PlayerController.GetPosition().y;
+		foreach(RoomActivator r in rooms) {
+			SetActiveState(y, r);
+		}
+	}
+
+	void SetActiveState(float y, RoomActivator r) {
+		r.gameObject.SetActive(y <= r.top && y > r.bottom);
+	}
+}
